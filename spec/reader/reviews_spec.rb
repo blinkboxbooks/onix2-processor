cd66cb6f64b06e08eff0e766407306addf6f5c49
JP Hastings-Spital <jphastings@blinkbox.com>
2014-10-27 14:05:03 +0000

    Tests for reviews

---
+++ spec/reader/reviews_spec.rb
@@ -0,0 +1,27 @@
+descriptor = ReaderExamples.add "reviews"
+
+RSpec.shared_examples descriptor do
+  describe "while reading #{descriptor}" do
+    it "must extract back cover text" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <product>
+          <othertext>
+            <d102>07</d102>
+            <d103>06</d103>
+            <d104>A review</d104>
+            <d107>J. G. Ballard</d107>
+          </othertext>
+        </product>
+      </ONIXmessage>
+      XML
+
+      expect((book['descriptions'] || []).size).to eq(0)
+      expect(book['reviews'].size).to eq(1)
+      rev = book['reviews'].first
+      expect(rev['type']).to eq("07")
+      expect(rev['content']).to eq("A review")
+      expect(rev['author']).to eq("J. G. Ballard")
+    end
+  end
+end
