421e7f8c05927bc9b7cd530dcd17113d489eb003
JP Hastings-Spital <jphastings@blinkbox.com>
2014-10-27 14:00:48 +0000

    Other Text - descriptions

---
+++ lib/blinkbox/onix2_processor/reader/descriptions.rb
@@ -0,0 +1,41 @@
+module Blinkbox::Onix2Processor
+  class OtherText <Processor
+    handles_xpath '/onixmessage/product/othertext'
+
+    def up(node, state)
+      @identifier = {}
+    end
+
+    def process(node, state)
+      position = normalize_tags(node.position)
+      container = position.last
+
+      unless node.node_type == Nokogiri::XML::Reader::TYPE_END_ELEMENT
+        content = (node.name == "#cdata-section" ? node.value : node.inner_xml).strip
+        @identifier[container] = content unless content.nil? || content.empty?
+      end
+    end
+
+    def down(node, state)
+      type = TYPES[@identifier['texttypecode']]
+
+      if !type.nil?
+        text = {
+          'type' => @identifier['texttypecode'],
+          'content' => @identifier['text']
+        }
+
+        text['author'] = @identifier['textauthor'] if @identifier['textauthor']
+
+        if text['content'] =~ /&(?:#?\d{2,3}|[a-z]+);/
+          text['content'] = HTMLEntities.new.decode(text['content'])
+        end
+        # Santize (HTML) output
+        text['content'] = sanitize_html(text['content'])
+        (state['book'][type] ||= []).push(text)
+      end
+    end
+
+    TYPES = YAML.load(open(File.join(__dir__, "../../../../config/othertext.yaml"))).freeze
+  end
+end
---
+++ spec/reader/descriptions_spec.rb
@@ -0,0 +1,133 @@
+descriptor = ReaderExamples.add "descriptions"
+
+RSpec.shared_examples descriptor do
+  describe "while reading #{descriptor}" do
+    it "must extract back cover text" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <OtherText>
+            <TextTypeCode>18</TextTypeCode>
+            <TextFormat>06</TextFormat>
+            <Text>My description</Text>
+          </OtherText>
+        </Product>
+      </ONIXmessage>
+      XML
+
+      expect(book['descriptions'].size).to eq(1)
+      desc = book['descriptions'].first
+      expect(desc['type']).to eq("18")
+      expect(desc['content']).to eq("My description")
+    end
+
+    it "must extract back cover html" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <OtherText>
+            <TextTypeCode>18</TextTypeCode>
+            <TextFormat>05</TextFormat>
+            <Text><![CDATA[<em>emphatically</em>]]></Text>
+          </OtherText>
+        </Product>
+      </ONIXmessage>
+      XML
+
+      expect(book['descriptions'].size).to eq(1)
+      desc = book['descriptions'].first
+      expect(desc['type']).to eq("18")
+      expect(desc['content']).to eq("<em>emphatically</em>")
+    end
+
+    it "must extract back cover with short tags" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <othertext>
+            <d102>18</d102>
+            <d103>06</d103>
+            <d104>A description</d104>
+          </othertext>
+        </Product>
+      </ONIXmessage>
+      XML
+
+      expect(book['descriptions'].size).to eq(1)
+      desc = book['descriptions'].first
+      expect(desc['type']).to eq("18")
+      expect(desc['content']).to eq("A description")
+    end
+
+    it "must extract and convert HTML entities" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <othertext>
+            <d102>18</d102>
+            <d103>05</d103>
+            <d104>This is &lt;strong&gt;html&lt;/strong&gt; dammit!</d104>
+          </othertext>
+        </Product>
+      </ONIXmessage>
+      XML
+
+      expect(book['descriptions'].size).to eq(1)
+      desc = book['descriptions'].first
+      expect(desc['content']).to eq("This is <strong>html</strong> dammit!")
+    end
+
+    it "must extract and convert HTML entities from within CDATA tags (ugh)" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <othertext>
+            <d102>18</d102>
+            <d103>05</d103>
+            <d104><![CDATA[This is &lt;strong&gt;html&lt;/strong&gt; dammit!]]></d104>
+          </othertext>
+        </Product>
+      </ONIXmessage>
+      XML
+
+      expect(book['descriptions'].size).to eq(1)
+      desc = book['descriptions'].first
+      expect(desc['content']).to eq("This is <strong>html</strong> dammit!")
+    end
+
+    it "must extract HTML classified as SGML" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <product>
+          <othertext>
+          <d102>01</d102>
+          <d103>01</d103>
+          <d104><![CDATA[This really isn't SGML]]></d104>
+          </othertext>
+        </product>
+      </ONIXmessage>
+      XML
+
+      expect(book['descriptions'].size).to eq(1)
+      desc = book['descriptions'].first
+      expect(desc['content']).to eq("This really isn't SGML")
+    end
+
+    it "must extract XHTML tags in the description" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <product>
+          <othertext>
+            <d102>02</d102>
+            <d104 textformat="05"><p>Howdy</p></d104>
+          </othertext>
+        </product>
+      </ONIXmessage>
+      XML
+
+      expect(book['descriptions'].size).to eq(1)
+      desc = book['descriptions'].first
+      expect(desc['content']).to eq("<p>Howdy</p>")
+    end
+  end
+end
