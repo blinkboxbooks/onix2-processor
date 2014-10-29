6f67be20e578ba6b20551e70a0111c6ef6ca14a3
JP Hastings-Spital <jphastings@blinkbox.com>
2014-10-29 17:12:50 +0000

    Add product type

--- spec/reader/epub_type_spec.rb
+++ spec/reader/epub_type_spec.rb
@@ -21,5 +21,45 @@ RSpec.shared_examples descriptor do
         end
       end
     end
+
+    it "must extract product form: DG" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <ProductForm>DG</ProductForm>
+        </Product>
+      </ONIXmessage>
+      XML
+      expect_schema_compliance(book)
+      expect(book["format"]["productForm"]).to eq("DG")
+      expect(book["format"]["marvinIncompatible"]).to eq(false)
+    end
+
+    %w{BA BB}.each do |form|
+      it "must extract product form: #{form}" do
+        book = process_xml_with_service <<-XML
+        <ONIXmessage>
+          <Product>
+            <ProductForm>#{form}</ProductForm>
+          </Product>
+        </ONIXmessage>
+        XML
+        expect_schema_compliance(book)
+        expect(book["format"]["productForm"]).to eq(form)
+        expect(book["format"]["marvinIncompatible"]).to eq(true)
+      end
+    end
+    
+    it "must not extract product form: 00" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <ProductForm>00</ProductForm>
+        </Product>
+      </ONIXmessage>
+      XML
+      expect_schema_compliance(book)
+      expect(book["format"]["productForm"]).to be_nil
+    end
   end
 end
