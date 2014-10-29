60399a636459ae780ceb17aa7455cfa9f6325db8
JP Hastings-Spital <jphastings@blinkbox.com>
2014-10-29 15:21:02 +0000

    Added dates

---
+++ spec/reader/dates_spec.rb
@@ -0,0 +1,53 @@
+descriptor = ReaderExamples.add "dates"
+
+RSpec.shared_examples descriptor do
+  describe "while reading #{descriptor}" do
+    it "must extract announcement dates" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <AnnouncementDate>20130101</AnnouncementDate>
+        </Product>
+      </ONIXmessage>
+      XML
+      expect_schema_compliance(book)
+      expect(book['dates']['announce']).to eq("2013-01-01")
+    end
+
+    it "must extract publish dates (year + month + day)" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <PublicationDate>20120202</PublicationDate>
+        </Product>
+      </ONIXmessage>
+      XML
+      expect_schema_compliance(book)
+      expect(book['dates']['publish']).to eq("2012-02-02")
+    end
+
+    it "must extract publish dates (year + month)" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <PublicationDate>201202</PublicationDate>
+        </Product>
+      </ONIXmessage>
+      XML
+      expect_schema_compliance(book)
+      expect(book['dates']['publish']).to eq("2012-02-01")
+    end
+
+    it "must extract publish dates (year)" do
+      book = process_xml_with_service <<-XML
+      <ONIXmessage>
+        <Product>
+          <PublicationDate>2012</PublicationDate>
+        </Product>
+      </ONIXmessage>
+      XML
+      expect_schema_compliance(book)
+      expect(book['dates']['publish']).to eq("2012-01-01")
+    end
+  end
+end
