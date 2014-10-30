663a36da39e7188d037be572e7c4a77a5d0c4f82
JP Hastings-Spital <jphastings@blinkbox.com>
2014-10-30 14:49:05 +0000

    Tests for cover images in ONIX

---
+++ spec/reader/cover_images_spec.rb
@@ -0,0 +1,30 @@
+descriptor = ReaderExamples.add "cover images"
+
+RSpec.shared_examples descriptor do
+  describe "while reading #{descriptor}" do
+    %w{04 06}.each do |front_cover_code|
+      it "must extract remote contributor images (type #{front_cover_code})" do
+        asset_url = "http://domain.com/path/to/image.jpg"
+        book = process_xml_with_service <<-XML
+        <ONIXmessage>
+          <Product>
+            <MediaFile>
+              <MediaFileTypeCode>#{front_cover_code}</MediaFileTypeCode>
+              <MediaFileLinkTypeCode>01</MediaFileLinkTypeCode>
+              <MediaFileLink>#{asset_url}</MediaFileLink>
+            </MediaFile>
+          </Product>
+        </ONIXmessage>
+        XML
+        expect_schema_compliance(book)
+        expect(book["media"]["images"].size).to eq(1)
+        img = book["media"]["images"].first
+        expect(img['classification']).to eq([{"realm" => "type", "id" => "front_cover"}])
+        expect(img['uris'].size).to eq(1)
+        url = img['uris'].first
+        expect(url['type']).to eq('remote')
+        expect(url['uri']).to eq(asset_url)
+      end
+    end
+  end
+end
