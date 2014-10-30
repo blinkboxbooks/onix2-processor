4773073f317ca14174386df5a2f2d5d64a022225
JP Hastings-Spital <jphastings@blinkbox.com>
2014-10-30 17:44:59 +0000

    To XML

--- lib/blinkbox/onix2_processor/processor.rb
+++ lib/blinkbox/onix2_processor/processor.rb
@@ -60,7 +60,7 @@ class Blinkbox::Onix2Processor::Processor
               error_message: e.message,
               position: position.join('/'),
               backtrace: e.backtrace.join("\n"),
-              node_xml: node.value
+              node_xml: node.to_xml
             }
           )
         end
