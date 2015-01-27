98acb04fe9594f1bd0950a9be4bc1a65663d5aec
JP Hastings-Spital <jphastings@blinkbox.com>
2015-01-27 18:02:23 +0000

    Remove tests for removed code

--- spec/reader/series_spec.rb
+++ spec/reader/series_spec.rb
@@ -15,30 +15,5 @@ context Blinkbox::Onix2Processor::Reader do
       expect(book['series']['title']).to eq("Battersea Dogs & Cats Home")
       expect(book['series']['number']).to eq(5)
     end
-
-    it "must extract series name and number from titles where appropriate" do
-      [
-        "Book name (Series name - book 2)",
-        "Book name (Series name - book two)",
-        "Book name (Series name book 2)",
-        "Book name: Series name Book 2",
-        "Book name: Series name Book Two",
-        "Book name (Series name, book 2)"
-      ].each do |title|
-        book = process_xml_with_service <<-XML
-        <ONIXmessage>
-          <Product>
-            <Title>
-              <TitleText>#{title}</TitleText>
-            </Title>
-          </Product>
-        </ONIXmessage>
-        XML
-        expect_schema_compliance(book)
-        expect(book['title']).to eq("Book name")
-        expect(book['series']['title']).to eq("Series name")
-        expect(book['series']['number']).to eq(2)
-      end
-    end
   end
 end
